a943ff319e1243d42cc3697ace49f6ca9ae45cd6
Swarup Das <78835981+swarupcs@users.noreply.github.com>
2025-04-15 17:58:11 +0530

    Theme setup

--- src/app/(main)/Navbar.tsx
+++ src/app/(main)/Navbar.tsx
@@ -1,12 +1,17 @@
 "use client";
 
 import logo from "@/assets/logo.png";
+import ThemeToggle from "@/components/ThemeToggle";
 import { UserButton } from "@clerk/nextjs";
+import { dark } from "@clerk/themes";
 import { CreditCard } from "lucide-react";
+import { useTheme } from "next-themes";
 import Image from "next/image";
 import Link from "next/link";
 
 export default function Navbar() {
+  const { theme } = useTheme();
+
   return (
     <header className="shadow-sm">
       <div className="mx-auto flex max-w-7xl items-center justify-between gap-3 p-3">
@@ -22,24 +27,28 @@ export default function Navbar() {
             AI Resume Builder
           </span>
         </Link>
-        <UserButton
-          appearance={{
-            elements: {
-              avatarBox: {
-                width: 35,
-                height: 35,
+        <div className="flex items-center gap-3">
+          <ThemeToggle />
+          <UserButton
+            appearance={{
+              baseTheme: theme === "dark" ? dark : undefined,
+              elements: {
+                avatarBox: {
+                  width: 35,
+                  height: 35,
+                },
               },
-            },
-          }}
-        >
-          <UserButton.MenuItems>
-            <UserButton.Link
-              label="Billing"
-              labelIcon={<CreditCard className="size-4" />}
-              href="/billing"
-            />
-          </UserButton.MenuItems>
-        </UserButton>
+            }}
+          >
+            <UserButton.MenuItems>
+              <UserButton.Link
+                label="Billing"
+                labelIcon={<CreditCard className="size-4" />}
+                href="/billing"
+              />
+            </UserButton.MenuItems>
+          </UserButton>
+        </div>
       </div>
     </header>
   );
